28fec4e909bd53cf401f97d75118840ff95dd4b8
FaLzNaufal <13520068@std.stei.itb.ac.id>
2023-03-01 03:07:17 +0700

    Try fix

--- reducer.py
+++ reducer.py
@@ -5,7 +5,7 @@ import csv
 counts = {}
 for line in sys.stdin:
     # remove leading and trailing whitespace
-    specific_resource_type, date = line.strip().split()
+    specific_resource_type, date = line.strip().split('\t')
     # increase counters
     if specific_resource_type not in counts:
         counts[specific_resource_type] = {}
@@ -13,10 +13,9 @@ for line in sys.stdin:
         counts[specific_resource_type][date] = 0
     counts[specific_resource_type][date] += 1
 
-
-for specific_resource_type in counts:
-    with open('output.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',')
-        writer.writerow(['specific_resource_type', 'date', 'count'])
-        for date in counts[specific_resource_type]:
-            writer.writerow([specific_resource_type, date, counts[specific_resource_type][date]])
+with open('output.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(['specific_resource_type', 'date', 'count'])
+    for specific_resource_type in counts:
+            for date in counts[specific_resource_type]:
+                writer.writerow([specific_resource_type, date, counts[specific_resource_type][date]])
