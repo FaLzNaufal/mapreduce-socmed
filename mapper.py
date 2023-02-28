695603c46d7e0cdeda5bd7f8825ddcaccf41e575
FaLzNaufal <13520068@std.stei.itb.ac.id>
2023-02-28 23:44:43 +0700

    Add other file handling

--- mapper.py
+++ mapper.py
@@ -4,13 +4,24 @@ import json
 import datetime
 import time
 
-with open('../raw_json/facebook_post_1641972260556_y83jramqg4.json', 'r', encoding='utf8') as f:
+with open('../raw_json/instagram_status_1642819881287.json', 'r', encoding='utf8') as f:
     file = json.load(f)
 
 # for line in sys.stdin:
 print(json.dumps(file, indent=4, sort_keys=True))
+# check if item is only one item or list of items
+if type(file) is dict:
+    file = [file]
 for item in file:
-    specific_resource_type = item['crawler_target']['specific_resource_type']
+    specific_resource_type = None
+    if 'crawler_target' in item:
+        specific_resource_type = item['crawler_target']['specific_resource_type']
+    if 'object' in item:
+        object = item['object']
+        if 'social_media' in object:
+            social_media = object['social_media']
+            if social_media == 'instagram':
+                specific_resource_type = 'instagram'
     if specific_resource_type == 'youtube':
         snippet = item['snippet']
         # check if comment is a reply
@@ -36,4 +47,18 @@ for item in file:
         for comment in comments:
             date = comment['created_time'][0:10]
             print(f'{specific_resource_type}\t{date}')
+    elif 'GraphImages' in item:
+        graphImages = item['GraphImages']
+        for image in graphImages:
+            date = image['taken_at_timestamp']
+            # format date from timestamp
+            date = datetime.datetime.fromtimestamp(int(date)).strftime('%Y-%m-%d')
+            print(f'instagram\t{date}')
+            comments = image['comments']['data']
+            for comment in comments:
+                date = comment['created_at']
+                # format date
+                date = datetime.datetime.fromtimestamp(int(date)).strftime('%Y-%m-%d')
+                print(f'instagram\t{date}')
+
 
