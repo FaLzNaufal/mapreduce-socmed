3db9e55a1185f2bb3df36e440e7c61c683cc15c2
FaLzNaufal <13520068@std.stei.itb.ac.id>
2023-03-01 08:23:35 +0700

    Add converter

---
+++ converter.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import json
+
+# convert .json files to .txt files
+input_path = "D:/ProjectKuliah/BigData/raw_json"
+output_path = "D:/ProjectKuliah/BigData/converted_txt"
+
+for filename in os.listdir(input_path):
+    with open(os.path.join(input_path, filename), 'r', encoding="utf8") as infile:
+        with open(os.path.join(output_path, filename[:-4] + "txt"), 'w', encoding="utf8") as outfile:
+            for line in infile:
+                json.dump(json.loads(line), outfile)
+                outfile.write('\n')
